19b1296bcbfdafb267167c0b6eeb1decfec679e7
Maxime <marceli11u@etu.univ-lorraine.fr>
2022-11-24 12:00:07 +0100

    Début de l'implémentation de l'API d'installation de la base de Données.

--- website/HTML/accueil.php
+++ website/HTML/accueil.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 	<head>
-		<title>Page Title</title>
+		<title>Cheap Marmiton</title>
 		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
 
