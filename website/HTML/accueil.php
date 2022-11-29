75c4c16bf78f3ea4126a96e9823ca39aada6dae6
Tysoun <tysoun2u@etu.univ-lorraine.fr>
2022-11-29 18:33:58 +0100

    Ajout de la page d'accueil

--- website/HTML/accueil.php
+++ website/HTML/accueil.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
-		<title>Cheap Marmiton</title>
+		<title>Cheap Marmiton | Recettes de cocktails</title>
 		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<link rel="stylesheet" href="./Style/style.css">
 	</head>
 	<body>
-
-		<h1>Hello World !</h1>
+		<header class="main-head">
+			<nav>
+			<div class="header">
+				<img src="../ressources/Img/logoCM4.jpg" alt="logo" />
+			</div>
+				<h1 id="logo">CheapMarmiton</h1>
+				<ul>
+					<li><a href="#">Se connecter</a> </li>
+					<li><a href="#">Créer un compte</a> </li>
+				</ul>
+			</nav>
+		</header>
 
 	</body>
 </html>
