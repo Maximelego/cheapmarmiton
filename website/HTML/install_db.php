786c1ec4b809bd89db977608ea27df3a4b96b016
Maxou <70325779+Maximelego@users.noreply.github.com>
2022-11-30 08:31:08 +0100

    Simplification du code grâce à l'utilisation de fonctions.

--- website/HTML/install_db.php
+++ website/HTML/install_db.php
@@ -2,29 +2,28 @@
 
 	require "Donnees.inc.php";
 
-	function checkIfElementExists($link,$element){
-		$found = true;
-
-		// -- Ingredients table -- //
-		$valuechanged = str_replace('"','""',$element);
+	function transformStringToSQLCompatible($string){
+		$valuechanged = str_replace('"','""',$string);
 		$valuechanged = str_replace("'","''",$valuechanged);
-		$query = "SELECT (id_element) FROM ELEMENTCATEGORIE WHERE (nom_element='$valuechanged');";
+		return $valuechanged;
+	}
+
+	function checkIfElementExists($link,$element,$table){
+		$found = true;
+		if(strcmp($table,"INGREDIENTS") == 0){
+			$query = "SELECT id_ingredient FROM $table WHERE (nom_ingredient='$element');";
+		} else if(strcmp($table,"ELEMENTCATEGORIE") == 0) {
+			$query = "SELECT id_element FROM $table WHERE (nom_element='$element');";
+		}
 		
 		$result = mysqli_query($link, $query);
 		$checkrows = mysqli_num_rows($result);
-		
 		if($checkrows == 0){
-			//DEBUG
-			//echo "$valuechanged is not in Elements !"."</br>";
 			$found = false;
 		}
 		mysqli_free_result($result);
-
 		return $found;
 	}
-
-
-
 
 	function query($link,$Sql){
 		if(isset($Sql) && strcmp($Sql,"") != 0){
@@ -82,48 +81,22 @@
 		// Recettes
 		foreach($Recettes as $key => $value){
 			// Special chars
-			$titre = str_replace('"','""',$value['titre']);
-			$titre = str_replace("'","''",$titre);
-
-			$ingredients = str_replace('"','""',$value['ingredients']);
-			$ingredients = str_replace("'","''",$ingredients);
-
-			$preparation = str_replace('"','""',$value['preparation']);
-			$preparation = str_replace("'","''",$preparation);
-
+			$titre = transformStringToSQLCompatible($value['titre']);
+			$ingredients = transformStringToSQLCompatible($value['ingredients']);
+			$preparation = transformStringToSQLCompatible($value['preparation']);
 
 			// Easy values
-			$Sql = "INSERT INTO RECETTES VALUES ($key,";
-			$Sql = $Sql."'".$titre."',";
-			$Sql = $Sql."'".$ingredients."',";
-			$Sql = $Sql."'".$preparation."');";
-
-			//DEBUG
-			//echo $Sql."</br>";
+			$Sql = "INSERT INTO RECETTES VALUES ($key,"."'".$titre."',"."'".$ingredients."',"."'".$preparation."');";
 			query($link, $Sql);
 
 			// Harder values
 			// -> index values
 			foreach($value['index'] as $keyindex => $valueindex){
-
-				// -- Ingredients table -- //
-				$valuechanged = str_replace('"','""',$valueindex);
-				$valuechanged = str_replace("'","''",$valuechanged);
-				$query = "SELECT (id_ingredient) FROM INGREDIENTS WHERE (nom_ingredient='$valuechanged');";
-
-				//DEBUG
-				//echo $query."</br>";
-
-				$result = mysqli_query($link, $query);
-				$checkrows = mysqli_num_rows($result);
-
-				if($checkrows == 0){
-					//DEBUG
-					//echo "$valuechanged is not in Ingredients !"."</br>";
+				$valuechanged = transformStringToSQLCompatible($valueindex);
+				if(!checkIfElementExists($link, $valuechanged, 'INGREDIENTS')){
 					$Sql = "INSERT INTO INGREDIENTS (nom_ingredient) VALUES ('$valuechanged');";
 					query($link, $Sql);
 				}
-				mysqli_free_result($result);
 
 				// -- Link table for index -- //
 				$query = "SELECT (id_ingredient) FROM INGREDIENTS WHERE (nom_ingredient='$valuechanged');";
@@ -132,46 +105,39 @@
 				$Sql = "INSERT INTO RECETTECONTIENTINGREDIENT VALUES ($key,$index[0]);";
 				query($link, $Sql);
 				mysqli_free_result($result);
-
 			}
 		}
 
 		// Hiérarchie
 		foreach($Hierarchie as $element => $caracteristiques){
 
-			if(!checkIfElementExists($link, $element)){
-				//DEBUG
-				echo "$valuechanged is not in Elements !"."</br>";
+			$valuechanged = transformStringToSQLCompatible($element);
+			if(!checkIfElementExists($link, $valuechanged,'ELEMENTCATEGORIE')){
 				$Sql = "INSERT INTO ELEMENTCATEGORIE (nom_element) VALUES ('$valuechanged');";
 				query($link, $Sql);
 			}
 
 			if(array_key_exists("sous_categorie",$caracteristiques)){
 				$souscat = $caracteristiques["sous-categorie"];
-				foreach($souscat as $key => $souselement){
-
-
-
-				}
+				
 			}
 			if(array_key_exists("super_categorie",$caracteristiques)){
 				$surcat = $caracteristiques["super-categorie"];
+
 			}
-			
-
-			// -- Implémentation de la sous-cat
-			//foreach($souscat as $key)
-			
-
-
 		}
 	}
 
+
+
+
+	// -- MAIN PROGRAM -- //
 	$link=mysqli_connect('127.0.0.1', 'root', '') or die("Erreur de connexion");
 	#Creation of database
 	buildDatabase($link);
 	#Adding data
 	implementData($link, $Recettes, $Hierarchie);
 	mysqli_close($link);
+	// ----------------- //
 
 ?>
