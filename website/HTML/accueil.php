3f24b291e540bf8cbd0b9307b2cf2a2f2b612591
Phil1010 <philliptysoun@gmail.com>
2022-12-02 10:37:44 +0100

    Icons
    Ajout icones.

--- website/HTML/accueil.php
+++ website/HTML/accueil.php
@@ -17,8 +17,8 @@
 			</div>
 			<h1 id="logo">CheapMarmiton</h1>
 			<ul>
-				<li><a href="connexion.php">Se connecter</a> </li>
-				<li><a href="favoris.php">Mes favoris</a> </li>
+				<li><a href="#"><img src="../ressources/Img/icons/login.png" alt="login" style="width:30px;height:30px;padding-left:-15px;margin-right: 7px;vertical-align:middle;margin-bottom:3px" />Se connecter</a> </li>
+				<li><a href="#"><img src="../ressources/Img/icons/favoris.png" alt="favoris" style="width:30px;height:30px;padding-left:-15px;margin-right: 7px;vertical-align:middle;margin-bottom:10px" />Mes favoris</a> </li>
 			</ul>
 		</nav>
 	</header>
@@ -30,23 +30,23 @@
 
 	<div class="wrapper">
 		<?php
-			$link = connectToDatabase();
-			query($link, "USE $base;");
-			$Sql = "SELECT * FROM RECETTES;";
-			$result = query($link, $Sql);
+		$link = connectToDatabase();
+		query($link, "USE $base;");
+		$Sql = "SELECT * FROM RECETTES;";
+		$result = query($link, $Sql);
 
-			while($index=mysqli_fetch_row($result)){
-				$image_name = scanTitle($index[1]);
-				if(file_exists("../ressources/Photos/$image_name.jpg")){
-					echo "<div class=\"box\">";
-					echo "<a href=\"recette.php?id_recette=$index[0]\">";
-					echo "<img src=\"../ressources/Photos/$image_name.jpg\" alt=\"$image_name\"/>"."</br>";
-					echo "<h2>"."$index[1]"."</h2>"."</br>";
-					echo "</a>";
-					echo "</div>";
-				}
+		while ($index = mysqli_fetch_row($result)) {
+			$image_name = scanTitle($index[1]);
+			if (file_exists("../ressources/Photos/$image_name.jpg")) {
+				echo "<div class=\"box\">";
+				echo "<a href=\"recette.php?id_recette=$index[0]\">";
+				echo "<img src=\"../ressources/Photos/$image_name.jpg\" alt=\"$image_name\"/>" . "</br>";
+				echo "<h2>" . "$index[1]" . "</h2>" . "</br>";
+				echo "</a>";
+				echo "</div>";
 			}
-			mysqli_close($link);
+		}
+		mysqli_close($link);
 		?>
 	</div>
 </body>
