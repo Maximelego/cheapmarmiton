944e6ec7838a1dee29a10d7792dacd75a113e481
Maxou <70325779+Maximelego@users.noreply.github.com>
2022-12-02 10:44:25 +0100

    Implémentation de recette.php
    Ajout ENCORE des redirections, ajout de la page d'affichage d'une recette.

--- website/HTML/accueil.php
+++ website/HTML/accueil.php
@@ -17,8 +17,8 @@
 			</div>
 			<h1 id="logo">CheapMarmiton</h1>
 			<ul>
-				<li><a href="#"><img src="../ressources/Img/icons/login.png" alt="login" style="width:30px;height:30px;padding-left:-15px;margin-right: 7px;vertical-align:middle;margin-bottom:3px" />Se connecter</a> </li>
-				<li><a href="#"><img src="../ressources/Img/icons/favoris.png" alt="favoris" style="width:30px;height:30px;padding-left:-15px;margin-right: 7px;vertical-align:middle;margin-bottom:10px" />Mes favoris</a> </li>
+				<li><a href="connexion.php"><img src="../ressources/Img/icons/login.png" alt="login" style="width:30px;height:30px;padding-left:-15px;margin-right: 7px;vertical-align:middle;margin-bottom:3px" />Se connecter</a> </li>
+				<li><a href="favoris.php"><img src="../ressources/Img/icons/favoris.png" alt="favoris" style="width:30px;height:30px;padding-left:-15px;margin-right: 7px;vertical-align:middle;margin-bottom:10px" />Mes favoris</a> </li>
 			</ul>
 		</nav>
 	</header>
--- website/HTML/recette.php
+++ website/HTML/recette.php
@@ -7,10 +7,43 @@
 		<link rel="stylesheet" type="text/css" href="Style/style.css?v=1">
 		<?php require "helper.php"; ?>
 	</head>
-        <h1> Hello world, i'm Recettes.php ! </h1>
 		<?php
-			echo $_GET['id_recette'];
-		
+			$id = $_GET['id_recette'];
+			$link = connectToDatabase();
+			$Sql = "USE $base";
+			query($link, $Sql);
+			$Sql = "SELECT * FROM RECETTES WHERE id_recette=$id;";
+			$result = query($link,$Sql);
+			$index = mysqli_fetch_row($result);
+
+			// -- Classical values -- //
+			foreach($index as $key => $value){
+				if($key != 0){
+					if($key ==  1){
+						echo "<h1>".$value."</h1>";
+					} else if($key == 2){
+						echo "<h2>"."Ingrédients : "."</h2>";
+						echo "<ul>";
+						foreach(explode("|",$value) as $str){
+							echo "<li>".$str."</li>";
+						}
+						echo "</ul>";
+					} else if($key == 3){
+						echo "<h2>"."Recette : "."</h2>";
+						echo "<h3>".$value."</h3>";
+					}
+				}
+			}
+
+			// -- Ingredients list -- //
+			/*echo "<ul>";
+			$Sql = "SELECT * FROM RECETTECONTIENTINGREDIENT INNER JOIN INGREDIENTS USING (id_ingredient) WHERE id_recette=$id;";
+			$result = query($link,$Sql);
+			while($index = mysqli_fetch_row($result)){
+				echo "<li>".$index[2]."</li>"."</br>";
+			}
+			echo "</ul>";*/
+
 		?>
 	</body>
 </html>
