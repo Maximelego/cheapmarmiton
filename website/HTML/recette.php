23a352532ff55266782cd266fc5d8f44cc7cb8ba
Maxou <70325779+Maximelego@users.noreply.github.com>
2022-12-24 10:49:53 +0100

    Rajout de la modification des favoris.
    Ajout d'un espacement des listes de recettes par rapport aux bords.
    Ajout de l'ajout et retrait des favoris.

--- website/HTML/recette.php
+++ website/HTML/recette.php
@@ -6,7 +6,27 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="Style/style.css?v=1">
-	<?php require "helper.php"; ?>
+	<?php require "helper.php"; 
+		// Initialize the session
+		session_start();
+		$connected = isUserConnected();
+
+		if(isset($_POST["add_recette"]) || isset($_POST["remove_recette"])){
+				// -- Favorites -- //
+				// -> if the user is connected
+				if($connected){
+					$id_user = $_SESSION["id"];
+				// -> if the user is not connected
+				} else {
+					$id_user = -1;
+				}
+			if(isset($_POST["add_recette"])){
+				addToFavorites($_POST["add_recette"],$id_user);
+			} else {
+				removeFromFavorites($_POST["remove_recette"],$id_user);
+			}
+		}
+	?>
 </head>
 
 <body>
@@ -30,11 +50,12 @@
 			</div>
 			<ul>
 				<?php
-				session_start();
-				if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+				if (!isUserConnected()) {
 					echo "<li><a href=\"connexion.php\"><img src=\"./ressources/Img/icons/login.png\" alt=\"login\" style=\"width:30px;height:30px;padding-left:-15px;margin-right: 7px;vertical-align:middle;margin-bottom:3px\" />Se connecter</a> </li>";
+					$id_user = -1;
 				} else {
 					echo "<li><a href=\"moncompte.php\"><img src=\"./ressources/Img/icons/login.png\" alt=\"login\" style=\"width:30px;height:30px;padding-left:-15px;margin-right: 7px;vertical-align:middle;margin-bottom:3px\" />Mon compte</a> </li>";
+					$id_user = $_SESSION["id"];
 				}
 				?>
 				<li><a href="favoris.php"><img src="./ressources/Img/icons/favoris.png" alt="favoris" style="width:30px;height:30px;padding-left:-15px;margin-right: 7px;vertical-align:middle;margin-bottom:10px" />Mes favoris</a> </li>
@@ -42,33 +63,57 @@
 		</nav>
 	</header>
 	<?php
-	$id = $_GET['id_recette'];
-	$link = connectToDatabase();
-	$Sql = "USE $base";
-	query($link, $Sql);
-	$Sql = "SELECT * FROM RECETTES WHERE id_recette=$id;";
-	$result = query($link, $Sql);
-	$index = mysqli_fetch_row($result);
+		$id = $_GET['id_recette'];
+		$link = connectToDatabase();
+		$Sql = "USE $base";
+		query($link, $Sql);
+		$Sql = "SELECT * FROM RECETTES WHERE id_recette=$id;";
+		$result = query($link, $Sql);
+		$index = mysqli_fetch_row($result);
 
-	// -- Classical values -- //
-	echo "<h1>" . $index[1] . "</h1>";
+		// -- Title and image -- //
+		echo "<h1>" . $index[1] . "</h1>";
+		$image_name = scanTitle($index[1]);
+		if (file_exists("./ressources/Photos/$image_name.jpg")) {
+			echo "<img src=\"./ressources/Photos/$image_name.jpg\" alt=\"$image_name\"/>" . "</br>";
+		} else {
+			echo "<img src=\"./ressources/Img/DEFAULT.png\" alt=\"DEFAULT\"/>";
+		}
 
-	$image_name = scanTitle($index[1]);
-	if (file_exists("./ressources/Photos/$image_name.jpg")) {
-		echo "<img src=\"./ressources/Photos/$image_name.jpg\" alt=\"$image_name\"/>" . "</br>";
-	} else {
-		echo "<img src=\"./ressources/Img/DEFAULT.png\" alt=\"DEFAULT\"/>";
-	}
-	echo "<h2>" . "Ingrédients : " . "</h2>";
-	echo "<ul>";
-	foreach (explode("|", $index[2]) as $str) {
-		echo "<li>" . $str . "</li>";
-	}
-	echo "</ul>";
-	echo "<h2>" . "Recette : " . "</h2>";
-	echo "<h3>" . $index[3] . "</h3>";
+		// -- Ingredients -- //
+		echo "<h2>" . "Ingrédients : " . "</h2>";
+		echo "<ul>";
+		foreach (explode("|", $index[2]) as $str) {
+			echo "<li>" . $str . "</li>";
+		}
+		echo "</ul>";
 
+		// -- Reciepe -- //
+		echo "<h2>" . "Recette : " . "</h2>";
+		foreach(explode(".",$index[3]) as $value){
+			if(!empty($value)){ echo "<h3>" . $value . "." . "</h3>"; }
+		}
 	?>
+
+	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+		<?php 
+			$id_recette = $_GET["id_recette"];
+			if($connected){
+				$id_user = $_SESSION["id"];
+			} else {
+				$id_user = -1;
+			}
+			$elementInBasket = checkIfReciepeinBasket($id_recette,$id_user);
+			if(!$elementInBasket){
+				echo "<input type=\"hidden\" name=\"add_recette\" value=$id_recette>";
+				echo "<input type=\"submit\" value=\"Ajouter aux favoris\">";
+			} else {
+				echo "<input type=\"hidden\" name=\"remove_recette\" value=$id_recette>";
+				echo "<input type=\"submit\" value=\"Retirer des favoris\">";
+			}
+		?>
+			
+	</form>
 </body>
 
 </html>
